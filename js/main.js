3e110cd3d366f82bc40098bb2638f2636e50529f
Markus Ylitalo <markus.ylitalo@phnet.fi>
2012-11-13 14:24:42 +0200

    Login

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
+window.serverUrl = 'http://lamk.net/api/';
 
+$(document).ready(function() {
+    
+    // initial login check
+    ajaxCall('logincheck');
+    
+    $("#login-form").submit(function(event) {
+        event.preventDefault();
+        
+        login();
+    });
+    
+});
+
+function login() {
+
+    var email = $("#login-email").val();
+    var pwd = hex_sha512($("#login-pwd").val());
+    
+    ajaxCall('login', email, pwd);
+}
+    
+function ajaxCall(act, email, password) {
+    
+    $.ajax({
+
+        url: window.serverUrl,
+        data: {
+            act: act,
+            email: email,
+            password: password
+        },
+        type: "GET",
+        dataType: 'jsonp',
+        crossDomain: true,
+        cache : "false",
+        success: function(data){
+            if (act == 'logincheck' && data.status == 0) {
+                $("#login-box").show();
+            } else if (act == 'login' && data.status == 1) {
+                $("#login-box").hide();
+            }
+        }
+
+    });
+
+}
